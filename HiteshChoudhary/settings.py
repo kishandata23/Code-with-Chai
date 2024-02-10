5f4feca766880adc96834085206272cc974d3b3e
kishan <kishandata23@gmail.com>
2024-02-10 12:24:14 +0530

    env working without user-db-pwd

--- HiteshChoudhary/settings.py
+++ HiteshChoudhary/settings.py
@@ -17,10 +17,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# from django-environ import environenv  
-# # reading .env file
-# environ.Env.read_env()
-
+from dotenv import load_dotenv
+load_dotenv()
 
 
 
@@ -28,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  os.environ('SECRET_KEY')
+SECRET_KEY =  os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,10 +85,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-        'USER': os.environ.get("DATABASE_USER"),
-        'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
-        'HOST': os.environ.get("DATABASE_HOST"),
-        'PORT': os.environ.get("DATABASE_PORT"),
+        # 'USER': os.environ.get("DATABASE_USER"),
+        # 'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
+        # 'HOST': os.environ.get("DATABASE_HOST"),
+        # 'PORT': os.environ.get("DATABASE_PORT"),
     }
 }
 
