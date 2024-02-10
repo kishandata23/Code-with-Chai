ab468440d24b66e555d3ac82c04a03876a4ef1df
kishan <kishandata23@gmail.com>
2024-02-10 12:03:50 +0530

    change to git-environ os package

--- HiteshChoudhary/settings.py
+++ HiteshChoudhary/settings.py
@@ -17,8 +17,9 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-from dotenv import load_dotenv
-load_dotenv()
+# from django-environ import environenv  
+# # reading .env file
+# environ.Env.read_env()
 
 
 
@@ -27,7 +28,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  os.environ.get('SECRET_KEY')
+SECRET_KEY =  os.environ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
