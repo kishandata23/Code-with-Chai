e836e44568b64e9c276343cfb9c3e7b8de9ce496
kishan <kishandata23@gmail.com>
2024-02-10 11:56:49 +0530

    added .sqlite3 by comment in .gitignore

--- HiteshChoudhary/settings.py
+++ HiteshChoudhary/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
         'USER': os.environ.get("DATABASE_USER"),
         'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
-        # 'HOST': os.environ.get("DATABASE_HOST"),
+        'HOST': os.environ.get("DATABASE_HOST"),
         'PORT': os.environ.get("DATABASE_PORT"),
     }
 }
